89909ba9342f6757b85a510c8c3fd868360a8da8
kimjaejeong <dyddkdlel33@naver.com>
2022-05-22 22:53:49 +0900

    코드 수정

--- exercise1/src/App.js
+++ exercise1/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 // import Hello from './component/Hello';
-import Hello from './component/Hello2';
+import Hello from './component/Hello3';
 import Welcome from './component/Welcome';
 import styles from "./App.module.css";
 
 function App(){
   return (<div className="App">
-    <Hello />
-    {/* <Welcome /> */}
-    {/* <div className={styles.box}>App</div> */}
+    <h3>pros : properties</h3>
+    <Hello age={10}/>
+    <Hello age={20}/>
+    <Hello age={30}/>
+
   </div>
   );
 }
--- exercise1/src/component/Hello2.js
+++ exercise1/src/component/Hello2.js
@@ -1,9 +1,30 @@
 export default function Hello(){
+    function showName(){
+        console.log("Mike");
+    }
+    function showAge(age){
+        console.log(age);
+    }
+
+    function showText(txt){
+        console.log(txt)
+
+    }
+
+
     return (
         <div>
             <h1>Hello</h1>
-            <button>Show name</button>
-            <button>Show age</button>
+            <button onClick={showName}>Show name</button>
+            <button 
+                onClick={() => {
+                   showAge(30)
+                }}
+            >Show age</button>
+            <input type = "text" onChange={(e) => {
+                const txt = e.target.value;
+                showText(txt);
+            }}/>
         </div>
     )
 }
